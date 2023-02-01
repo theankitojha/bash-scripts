49bba8c40f333b08e02b7f22c6dfb8a805f99d73
Ankit Ojha <theankitojha@gmail.com>
2023-02-01 22:22:33 +0545

    push

---
+++ swap-inc.sh
@@ -0,0 +1,26 @@
+######################################################################
+##
+##   Swap Memory Add Script
+##   Written By: Ankit Ojha
+##   URL: ankitojha.com.np
+##   Update on: Feb 1, 2023
+##
+######################################################################
+#!/bin/bash
+echo "-----------------------------"
+echo "SWAP MEMORY IS BEING ADDED !!"
+echo "-----------------------------"
+echo ""
+read -p "Enter SWAP SIZE to add:(e.g. for 2GB -> type 2G and Enter" size
+sudo fallocate -l $size /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+echo "---------------------------------"
+echo "SWAP MEMORY ADDED SUCCESSFULLY !!"
+echo "---------------------------------"
